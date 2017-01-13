752d0f51febbaa13c8e81f60f3ac3dd11ec5e2bf
Thinh Le <timothyle85@gmail.com>
2017-01-12 21:39:34 -0800

    Creating main.js file
    JavaScript enhanced with jquery for better gui on website

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/*
+function main(){
+  $('.skillset').hide();
+  $('.projects').hide();
+  $('.projects-button').on('click', function(){
+   // $(this).next().toggle();
+    $(this).toggleClass('active');
+    $(this).text('Projects Viewed');
+    $(this).next().slideToggle(400);
+  });
+  
+  $('.skillset').fadeIn(400);
+}
+$(document).ready(main);
+*/
